692323365ffe069afe4bbe62d086c21ffa0cdd8c
yunjiesong <yunjiesong01@gmail.com>
2017-11-27 22:40:32 +0800

    download example

---
+++ ebi.download.sh
@@ -0,0 +1,1 @@
+ascp -QT -l 300m -P33001 -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@fasp.sra.ebi.ac.uk:/vol1/fastq/ERR036/ERR036000/ERR036000_1.fastq.gz . 
---
+++ ncbi.download.sh
@@ -0,0 +1,1 @@
+ascp -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh -T anonftp@ftp-private.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/SRR/SRR689/SRR689250/SRR689250.sra ./ 
